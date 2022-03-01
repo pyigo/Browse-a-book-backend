58fc32dbf21ef12516b8c9200f79e8a8a133129f
paulette <paulette.yigo@gmail.com>
2022-02-28 22:16:25 -0500

    initial commit

---
+++ src/main/java/com/pauyigo/capstone/SpringcapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.pauyigo.capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcapstoneApplication.class, args);
+	}
+
+}
