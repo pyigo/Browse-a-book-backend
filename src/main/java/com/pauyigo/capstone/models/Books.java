10339abed08236dcd70165b3908a8e4ffd3fc92f
paulette <paulette.yigo@gmail.com>
2022-02-28 23:18:41 -0500

    created Books class models, generated methods

--- src/main/java/com/pauyigo/capstone/models/Books.java
+++ src/main/java/com/pauyigo/capstone/models/Books.java
@@ -1,5 +1,63 @@
 package com.pauyigo.capstone.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="books")
 public class Books {
 
+	@Id
+	@Column
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	@Column
+	private String title;
+	@Column
+	private String description;
+	@Column
+	private String publisedDate;
+	@Column
+	private String author;
+	@Column
+	private String category;
+	public int getId() {
+		return id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getPublisedDate() {
+		return publisedDate;
+	}
+	public void setPublisedDate(String publisedDate) {
+		this.publisedDate = publisedDate;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
 }
