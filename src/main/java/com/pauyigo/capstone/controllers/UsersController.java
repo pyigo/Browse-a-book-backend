7fd23a5ede333586dde8884f88f3a4f273ae2af2
paulette <paulette.yigo@gmail.com>
2022-03-03 00:03:35 -0500

    created DTO to avoid exposing client sensitive info

--- src/main/java/com/pauyigo/capstone/controllers/UsersController.java
+++ src/main/java/com/pauyigo/capstone/controllers/UsersController.java
@@ -1,5 +1,6 @@
 package com.pauyigo.capstone.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pauyigo.capstone.dto.UserToReturn;
 import com.pauyigo.capstone.models.Users;
 import com.pauyigo.capstone.repositories.UsersRepository;
 
@@ -23,8 +25,24 @@ public class UsersController {
 	private UsersRepository usersRepo;
 
 	@GetMapping("allusers")
-	public List<Users> getAllusers() {
-		return usersRepo.findAll();
+	public List<UserToReturn> getAllusers() {
+		
+		List <Users> dbUsers = usersRepo.findAll();
+		List<UserToReturn> usersToReturn = new ArrayList<>();
+		
+		for (Users dbuser : dbUsers) 
+		{ 
+			UserToReturn userToReturn= new UserToReturn();
+			userToReturn.setId(dbuser.getId());
+			userToReturn.setFirstname(dbuser.getFirstname());
+			userToReturn.setLastname(dbuser.getLastname());
+			userToReturn.setUsername(dbuser.getUsername());
+			userToReturn.setEmail(dbuser.getEmail());
+			
+		    usersToReturn.add(userToReturn);
+		}
+		
+		return usersToReturn;
 	}
 
 	// ResponseEntity represents an HTTP response, including headers, body, and
